142d8226352c77c613534e54cc7baf86bb6d19d5
namchuminh <chuminhnamma@gmail.com>
2023-06-07 17:07:35 +0700

    dang nhap, dang xuat, phan quyen trong quan ly

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,12 @@
 const User = require("../models/userModel.js");
 
 class loginController{
+    //[GET] dang-nhap/quan-ly
     index(req, res) {
         res.render("login/adminLogin");
     }
 
+    //[GET] dang-xuat/quan-ly
     logout(req, res){
         req.logout((err) => {
             if (err) {
